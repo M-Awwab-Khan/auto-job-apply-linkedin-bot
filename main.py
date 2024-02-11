aef471607c06b1a7f2b285dfc3e7494991e0e2fb
Awwab <mawwabkhank2006@gmail.com>
2024-02-11 10:32:20 +0500

    got bot to click on signin

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ chrome_options.add_experimental_option('detach', True)
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.linkedin.com/jobs/search/?currentJobId=3810377786&f_AL=true&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&origin=JOB_SEARCH_PAGE_LOCATION_AUTOCOMPLETE&refresh=true')
+
+signin = driver.find_element(By.LINK_TEXT, 'Sign in')
+signin.click()
